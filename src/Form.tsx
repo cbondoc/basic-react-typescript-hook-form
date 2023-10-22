947185b1225015997a5e61cacd10fef740f571d3
Cary <cbondoc.it@gmail.com>
2023-10-22 22:06:23 +0800

    feat: added successful message

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 export default function Form() {
+  const [isSubmitted, setIsSubmitted] = useState(false);
   const {
     register,
     handleSubmit,
@@ -15,64 +16,72 @@ export default function Form() {
     },
   });
 
+  const onSubmit = (data: any) => {
+    console.log(data);
+    setIsSubmitted(true);
+  };
+
   return (
-    <form onSubmit={handleSubmit((data) => console.log(data))}>
-      <div>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          {...register("username", {
-            required: "Username is required",
-            minLength: {
-              value: 5,
-              message: "The username should have at least 5 characters",
-            },
-            pattern: {
-              value: /^[a-zA-Z0-9_]+$/,
-              message: "Username must contain only letters, numbers, and _",
-            },
-          })}
-        />
-        {errors.username && <small>{errors.username.message}</small>}
-      </div>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            {...register("username", {
+              required: "Username is required",
+              minLength: {
+                value: 5,
+                message: "The username should have at least 5 characters",
+              },
+              pattern: {
+                value: /^[a-zA-Z0-9_]+$/,
+                message: "Username must contain only letters, numbers, and _",
+              },
+            })}
+          />
+          {errors.username && <small>{errors.username.message}</small>}
+        </div>
 
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          {...register("email", {
-            required: "Email is required",
-            maxLength: {
-              value: 50,
-              message: "The email should have at most 50 characters",
-            },
-            pattern: {
-              value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
-              message: "Email address must be a valid address",
-            },
-          })}
-        />
-        {errors.email && <small>{errors.email.message}</small>}
-      </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            {...register("email", {
+              required: "Email is required",
+              maxLength: {
+                value: 50,
+                message: "The email should have at most 50 characters",
+              },
+              pattern: {
+                value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+                message: "Email address must be a valid address",
+              },
+            })}
+          />
+          {errors.email && <small>{errors.email.message}</small>}
+        </div>
 
-      <div>
-        <label htmlFor="isAdmin">IsAdmin</label>
-        <input id="isAdmin" type="checkbox" {...register("isAdmin")} />
-      </div>
+        <div>
+          <label htmlFor="isAdmin">IsAdmin</label>
+          <input id="isAdmin" type="checkbox" {...register("isAdmin")} />
+        </div>
 
-      <div>
-        <label htmlFor="createdAt">Creation Date</label>
-        <input
-          id="createdAt"
-          type="date"
-          {...register("createdAt", {
-            required: "Date is required",
-          })}
-        />
-        {errors.createdAt && <small>{errors.createdAt.message}</small>}
-      </div>
+        <div>
+          <label htmlFor="createdAt">Creation Date</label>
+          <input
+            id="createdAt"
+            type="date"
+            {...register("createdAt", {
+              required: "Date is required",
+            })}
+          />
+          {errors.createdAt && <small>{errors.createdAt.message}</small>}
+        </div>
 
-      <button type="submit">Submit</button>
-    </form>
+        <button type="submit">Submit</button>
+      </form>
+      {isSubmitted && <div>Form submitted successfully!</div>}
+    </div>
   );
 }
